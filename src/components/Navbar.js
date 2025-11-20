44bdb2bbc4c787e0a57e5c48293b85b9a9a5a6f3
Mai Hamed <maiabostate2005@gmail.com>
2025-11-20 21:12:28 +0200

    fixed navbar and added globe and cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,26 @@ function Navbar({ navLinks }) {
       <div className="left">
         <h1 className="logo">Udemy</h1>
         <span className="explore">Explore</span>
-        <input className="search" placeholder="Search for anything" />
+        <div className="search-container">
+          <input className="search" placeholder="Search for anything" />
+        </div>
       </div>
 
       <div className="right">
         {navLinks.map((item, index) => (
-          <a key={index} href={item.url}>{item.label}</a>
+          <a key={index} href={item.url} className="nav-link">{item.label}</a>
         ))}
+        
+        <div className="nav-icons">
+          <img src="/images/shopping-cart.png" alt="Cart" className="nav-icon" />
+        </div>
 
         <button className="login">Log in</button>
         <button className="signup">Sign up</button>
+        <div className="nav-icons">
+          <img src="/images/globe.png" alt="Language" className="nav-icon" />
+        </div>
+
       </div>
     </nav>
   );

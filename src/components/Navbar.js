87bd3b106e45c87b772400a1fc67a10fd5dca1f5
Mai Hamed <maiabostate2005@gmail.com>
2025-11-20 22:27:03 +0200

    fix navbar spacing and added the actual logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ function Navbar({ navLinks }) {
   return (
     <nav className="navbar">
       <div className="left">
-        <h1 className="logo">Udemy</h1>
+        <img src="/images/logo_udemy.svg" alt="Udemy" className="logo" />
         <span className="explore">Explore</span>
         <div className="search-container">
           <input className="search" placeholder="Search for anything" />
@@ -16,16 +16,10 @@ function Navbar({ navLinks }) {
           <a key={index} href={item.url} className="nav-link">{item.label}</a>
         ))}
         
-        <div className="nav-icons">
-          <img src="/images/shopping-cart.png" alt="Cart" className="nav-icon" />
-        </div>
-
+        <img src="/images/shopping-cart.png" alt="Cart" className="nav-icon" />
         <button className="login">Log in</button>
         <button className="signup">Sign up</button>
-        <div className="nav-icons">
-          <img src="/images/globe.png" alt="Language" className="nav-icon" />
-        </div>
-
+        <img src="/images/globe.png" alt="Language" className="nav-icon" />
       </div>
     </nav>
   );
